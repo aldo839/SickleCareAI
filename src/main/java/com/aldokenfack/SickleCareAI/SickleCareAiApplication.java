fa9e27b7b05e00c1891826c34bb2bf11ae8d0482
aldo839 <christianaldo515@gmail.com>
2026-07-17 15:46:43 +0100

    Initial commit

---
+++ src/main/java/com/aldokenfack/SickleCareAI/SickleCareAiApplication.java
@@ -0,0 +1,13 @@
+package com.aldokenfack.SickleCareAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SickleCareAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SickleCareAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldokenfack/SickleCareAI/SickleCareAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldokenfack.SickleCareAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SickleCareAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
