31013e44aeafb6a48d98dd476ef523ff247b1807
aldo839 <christianaldo515@gmail.com>
2026-07-20 15:48:47 +0100

    Creation of Doctor entity and his variables

---
+++ src/main/java/com/aldokenfack/SickleCareAI/model/Doctor.java
@@ -0,0 +1,29 @@
+package com.aldokenfack.SickleCareAI.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Doctor {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String speciality;
+
+    private String matricule;
+
+    private String validationLetterReference;
+
+    private String hospitalUnit;
+
+    private String region;
+
+    private String city;
+
+    private String hospital;
+
+}
