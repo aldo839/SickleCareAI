cf0f65118439ccdbcb4a32909db6431889248bb3
aldo839 <christianaldo515@gmail.com>
2026-07-21 14:15:53 +0100

    Implementation of the DoctorMapperService with the purpose to hide some data

---
+++ src/main/java/com/aldokenfack/SickleCareAI/service/DoctorMapperService.java
@@ -0,0 +1,29 @@
+package com.aldokenfack.SickleCareAI.service;
+
+import com.aldokenfack.SickleCareAI.dto.DoctorResponseDTO;
+import com.aldokenfack.SickleCareAI.model.Doctor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoctorMapperService {
+
+    public DoctorResponseDTO mapToResponseDTO(Doctor doctor){
+
+        return new DoctorResponseDTO(
+
+                doctor.getUsername(),
+                doctor.getEmail(),
+                doctor.getFirstname(),
+                doctor.getLastname(),
+                doctor.getSpeciality(),
+                doctor.getMatricule(),
+                doctor.getValidationLetterReference(),
+                doctor.getHospitalUnit(),
+                doctor.getRegion(),
+                doctor.getCity(),
+                doctor.getHospital()
+        );
+
+    }
+
+}
