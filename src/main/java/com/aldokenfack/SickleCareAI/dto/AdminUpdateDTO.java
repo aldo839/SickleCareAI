8b82e27698d33b017b8ea643977fb231d4180d23
aldo839 <christianaldo515@gmail.com>
2026-07-21 15:52:16 +0100

    Creation of AdminUpdateDTO with his validation requirement

---
+++ src/main/java/com/aldokenfack/SickleCareAI/dto/AdminUpdateDTO.java
@@ -0,0 +1,24 @@
+package com.aldokenfack.SickleCareAI.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AdminUpdateDTO {
+
+    @NotBlank(message = "Username is require !")
+    @Size(min = 4, max = 20, message = "Username must have between 4 and 20 characters !")
+    private String username;
+
+    @Email(message = "Email must be valid !")
+    @NotBlank(message = "Email is require !")
+    private String email;
+
+    @NotBlank(message = "Password is require !")
+    @Size(min = 8, message = "Password must have minimum 8 characters !")
+    private String password;
+
+}
