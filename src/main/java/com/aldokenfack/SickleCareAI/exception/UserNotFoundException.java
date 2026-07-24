09eeab1da0dd7f2e8e24ee43c622b2b51f5966ea
aldo839 <christianaldo515@gmail.com>
2026-07-24 19:21:57 +0100

    Creation of the UserNotFoundException for customized message in the case of the user not found error.

---
+++ src/main/java/com/aldokenfack/SickleCareAI/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.aldokenfack.SickleCareAI.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
