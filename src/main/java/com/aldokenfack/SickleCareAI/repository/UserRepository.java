af6fe3ab16a014fb29ca60d4112176b7e7f86564
aldo839 <christianaldo515@gmail.com>
2026-07-26 09:58:48 +0100

    Modification of the method findByEmail to type Optional.

--- src/main/java/com/aldokenfack/SickleCareAI/repository/UserRepository.java
+++ src/main/java/com/aldokenfack/SickleCareAI/repository/UserRepository.java
@@ -4,12 +4,14 @@ import com.aldokenfack.SickleCareAI.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     Boolean existsByUsername(String username);
 
