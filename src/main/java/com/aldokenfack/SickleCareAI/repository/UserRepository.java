b28d35efaa0b3525109b25449aa1d81853dcadd5
aldo839 <christianaldo515@gmail.com>
2026-07-26 08:46:22 +0100

    Adding of the method findByEmail (We will use email and password for authentication).

--- src/main/java/com/aldokenfack/SickleCareAI/repository/UserRepository.java
+++ src/main/java/com/aldokenfack/SickleCareAI/repository/UserRepository.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
