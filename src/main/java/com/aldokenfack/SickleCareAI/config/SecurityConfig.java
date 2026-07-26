13070e80ef3a28c7e6183d341040afbd7cbdd6fd
aldo839 <christianaldo515@gmail.com>
2026-07-26 08:55:34 +0100

    Implementation of the method AuthenticationManager as a bean. The purpose od this method is to manage user authentication by loading UserService, encode the user input password, compare the UserDetails loaded before with the user input credentials.

--- src/main/java/com/aldokenfack/SickleCareAI/config/SecurityConfig.java
+++ src/main/java/com/aldokenfack/SickleCareAI/config/SecurityConfig.java
@@ -1,28 +1,51 @@
 package com.aldokenfack.SickleCareAI.config;
 
+import com.aldokenfack.SickleCareAI.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
-        return http.build();
+        return http
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests(auth ->
+                                auth.requestMatchers(HttpMethod.POST, "/patients", "/doctors", "/user/login").permitAll()
+                                        .requestMatchers(HttpMethod.POST, "/admin").hasRole("ROOT")
+                                        .anyRequest().authenticated()
+                        ).build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder){
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+
+        return authenticationManagerBuilder.build();
     }
 
 }
