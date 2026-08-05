4313f1d4f813760140b11ff0d1d45d50b69984a0
aldo839 <christianaldo515@gmail.com>
2026-08-05 21:29:44 +0100

    Adding of the optional method findByCode to our ValidationRepository

--- src/main/java/com/aldokenfack/SickleCareAI/repository/ValidationRepository.java
+++ src/main/java/com/aldokenfack/SickleCareAI/repository/ValidationRepository.java
@@ -3,5 +3,10 @@ package com.aldokenfack.SickleCareAI.repository;
 import com.aldokenfack.SickleCareAI.model.Validation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ValidationRepository extends JpaRepository<Validation, Long> {
+
+    Optional<Validation> findByCode(String code);
+
 }
