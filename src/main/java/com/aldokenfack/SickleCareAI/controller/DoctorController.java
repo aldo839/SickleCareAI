c77894e765013fa8076f3d64b242e832873c7ac9
aldo839 <christianaldo515@gmail.com>
2026-08-05 21:39:53 +0100

    implementation of the POST method activateDoctor that use a Map of activation as a requestBody and return a string for account activation status.

--- src/main/java/com/aldokenfack/SickleCareAI/controller/DoctorController.java
+++ src/main/java/com/aldokenfack/SickleCareAI/controller/DoctorController.java
@@ -5,21 +5,20 @@ import com.aldokenfack.SickleCareAI.dto.DoctorResponseDTO;
 import com.aldokenfack.SickleCareAI.dto.DoctorUpdateDTO;
 import com.aldokenfack.SickleCareAI.service.DoctorService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/doctors")
 public class DoctorController {
 
     private final DoctorService doctorService;
-
-    public DoctorController(DoctorService doctorService) {
-        this.doctorService = doctorService;
-    }
 
 
     @GetMapping
@@ -36,10 +35,19 @@ public class DoctorController {
     }
 
 
-    @PostMapping
+    @PostMapping("/register-doctor")
     public ResponseEntity<DoctorResponseDTO> registerDoctor(@Valid @RequestBody DoctorRegistrationDTO dto){
 
         return new ResponseEntity<>(doctorService.registerDoctor(dto), HttpStatus.CREATED);
+    }
+
+
+    @PostMapping("/activate-doctor")
+    public ResponseEntity<String> activateDoctor(@RequestBody Map<String, String> activation){
+
+        String status = doctorService.activation(activation);
+
+        return ResponseEntity.ok("Account successfully activate. \nStatus : " + status);
     }
 
 
@@ -56,4 +64,5 @@ public class DoctorController {
         doctorService.deleteDoctor(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
