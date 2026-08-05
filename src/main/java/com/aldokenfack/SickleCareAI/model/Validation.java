ee929f253814a471cb565992ed0686b26dcc2762
aldo839 <christianaldo515@gmail.com>
2026-08-05 07:02:25 +0100

    Creation of the entity Validation and his variables to manage validation code

---
+++ src/main/java/com/aldokenfack/SickleCareAI/model/Validation.java
@@ -0,0 +1,29 @@
+package com.aldokenfack.SickleCareAI.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+public class Validation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant creation;
+
+    private Instant expiration;
+
+    private Instant activation;
+
+    private String code;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+
+}
