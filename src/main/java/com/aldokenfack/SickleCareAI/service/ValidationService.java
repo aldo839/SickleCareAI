ebbdd34e2fb4a29312d4e0010f0e7c89d28f39df
aldo839 <christianaldo515@gmail.com>
2026-08-05 07:04:43 +0100

    Creation of the ValidationService and implementation of the method registerUser to generate and save the information about validation code

---
+++ src/main/java/com/aldokenfack/SickleCareAI/service/ValidationService.java
@@ -0,0 +1,42 @@
+package com.aldokenfack.SickleCareAI.service;
+
+import com.aldokenfack.SickleCareAI.model.User;
+import com.aldokenfack.SickleCareAI.model.Validation;
+import com.aldokenfack.SickleCareAI.repository.ValidationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
+@RequiredArgsConstructor
+public class ValidationService {
+
+    private final ValidationRepository validationRepository;
+    private final NotificationService notificationService;
+
+    public void registerUser(User user){
+
+        Validation validation = new Validation();
+
+        Instant creation = Instant.now();
+        Instant expiration = creation.plus(15, ChronoUnit.MINUTES);
+
+        SecureRandom random = new SecureRandom();
+        Integer randomInteger = random.nextInt(999999);
+        String code = String.format("%06d", randomInteger);
+
+        validation.setUser(user);
+        validation.setCreation(creation);
+        validation.setExpiration(expiration);
+        validation.setCode(code);
+
+        validationRepository.save(validation);
+
+        notificationService.sendActivationMessage(validation);
+
+    }
+
+}
