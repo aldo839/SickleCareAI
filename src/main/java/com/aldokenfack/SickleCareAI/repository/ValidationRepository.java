c9720026490f7814f5eac0ca12b0fc60f0d36f6f
aldo839 <christianaldo515@gmail.com>
2026-08-05 07:03:14 +0100

    Creation of the ValidationRepository for information about Validation

---
+++ src/main/java/com/aldokenfack/SickleCareAI/repository/ValidationRepository.java
@@ -0,0 +1,7 @@
+package com.aldokenfack.SickleCareAI.repository;
+
+import com.aldokenfack.SickleCareAI.model.Validation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ValidationRepository extends JpaRepository<Validation, Long> {
+}
