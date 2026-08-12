cbe8dc528d444a7dec3164433894d2d52e3c59c3
aldo839 <christianaldo515@gmail.com>
2026-08-12 04:21:13 +0100

    Adding of unprotected route for all the methods of the authController.

--- src/main/java/com/aldokenfack/SickleCareAI/config/SecurityConfig.java
+++ src/main/java/com/aldokenfack/SickleCareAI/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.aldokenfack.SickleCareAI.config;
 
+import com.aldokenfack.SickleCareAI.filter.JwtFilter;
 import com.aldokenfack.SickleCareAI.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
+    private final JwtUtils jwtUtils;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,11 +30,14 @@ public class SecurityConfig {
         return http
                         .csrf(AbstractHttpConfigurer::disable)
                         .authorizeHttpRequests(auth -> auth
-                                .requestMatchers(HttpMethod.POST, "/api/patients", "/api/doctors/", "/api/users/login").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/api/admin").hasRole("ROOT")
-                                .requestMatchers(HttpMethod.DELETE, "/api/patients", "/api/doctors/", "/api/admin").hasRole("ROOT")
-                                .anyRequest().authenticated()
-                        ).build();
+
+                                .requestMatchers(HttpMethod.POST, "/api/auth/*").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/patients/register-patient", "/api/doctors/register-doctor").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/patients/activate-patient", "/api/doctors/activate-doctor").permitAll()
+                                .anyRequest().permitAll()
+                        )
+                        .addFilterBefore(new JwtFilter(jwtUtils, userDetailsService), UsernamePasswordAuthenticationFilter.class)
+                        .build();
     }
 
     @Bean
