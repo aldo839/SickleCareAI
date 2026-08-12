e8d72d8d95e5c6694a71a64434ce1221b88c3866
aldo839 <christianaldo515@gmail.com>
2026-08-12 04:19:34 +0100

    Replacing of the annotation UserService by AuthService

--- src/main/java/com/aldokenfack/SickleCareAI/service/AuthService.java
+++ src/main/java/com/aldokenfack/SickleCareAI/service/AuthService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class AuthService {
 
     private final UserMapperService userMapperService;
     private final UserRepository userRepository;
