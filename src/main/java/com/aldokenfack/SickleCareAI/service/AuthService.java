ffb258a6f7e65bdc39cd0a90d56801b020ea2041
aldo839 <christianaldo515@gmail.com>
2026-08-19 10:16:44 +0100

    Creation of the method getCurrentUser with the type User. The purpose is to fetch the email of the user after his authentication and use it to fetch the details of the user. We will use this user details (user id) to apply and access control check on any route that take an id inside the URL to avoid IDOR vulnerability.

--- src/main/java/com/aldokenfack/SickleCareAI/service/AuthService.java
+++ src/main/java/com/aldokenfack/SickleCareAI/service/AuthService.java
@@ -4,18 +4,17 @@ import com.aldokenfack.SickleCareAI.config.JwtUtils;
 import com.aldokenfack.SickleCareAI.dto.UserLoginRequestDTO;
 import com.aldokenfack.SickleCareAI.dto.UserLoginResponseDTO;
 import com.aldokenfack.SickleCareAI.exception.UserNotFoundException;
+import com.aldokenfack.SickleCareAI.model.JwtRefreshToken;
 import com.aldokenfack.SickleCareAI.model.User;
 import com.aldokenfack.SickleCareAI.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -25,20 +24,39 @@ public class AuthService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
-
+    private final JwtRefreshTokenService jwtRefreshTokenService;
 
     public UserLoginResponseDTO loginUser(UserLoginRequestDTO dto){
-
-        try{
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword()));
-        } catch (AuthenticationException e) {
-            throw new UserNotFoundException("Bad email or password. Please try again !");
-        }
 
         User user = userRepository.findByEmail(dto.getEmail())
                 .orElseThrow(() -> new UserNotFoundException("User not found !"));
 
-        return userMapperService.mapToResponseDTO(user);
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword()));
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Bad email or password. Please try again !");
+        }
+
+        String accessToken = jwtUtils.generateToken(user.getEmail());
+        JwtRefreshToken refreshToken = jwtRefreshTokenService.generateRefreshToken(user.getId());
+
+        return userMapperService.mapToResponseDTO(user, accessToken, refreshToken.getToken());
+    }
+
+
+    public User getCurrentUser(){
+
+        // Fetch the email inside the JWT
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()){
+            throw new BadCredentialsException("User not authenticated");
+        }
+
+        String email = authentication.getName();
+        // Fetch the user with this email
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User not found with email : " + email));
     }
 
 }
