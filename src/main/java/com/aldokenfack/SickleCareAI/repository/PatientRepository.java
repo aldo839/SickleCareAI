f4a356598c337edee0de8f2d4d0d27f80bd9d55f
aldo839 <christianaldo515@gmail.com>
2026-08-19 11:10:01 +0100

    Creation of the method findByDoctorID on PatientRepository to fetch all the patient follow by one specific doctor.

--- src/main/java/com/aldokenfack/SickleCareAI/repository/PatientRepository.java
+++ src/main/java/com/aldokenfack/SickleCareAI/repository/PatientRepository.java
@@ -4,11 +4,15 @@ import com.aldokenfack.SickleCareAI.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
+
+    Optional<Patient> findByDoctorId(Long doctorId);
 
 }
