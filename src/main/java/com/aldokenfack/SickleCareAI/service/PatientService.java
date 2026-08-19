08835d699c6231b5f08367a18472e4e3cc0321da
aldo839 <christianaldo515@gmail.com>
2026-08-19 11:08:28 +0100

    Creation of the method validatePatient that will be used by the admin to validate patient account.
    Modification of the method updatePatient to use the currentUserId to verify if the user to update is the connected user.
    Creation and implementation of the method selectDoctor that also use the verification of the user who want to select his doctor with the currentUserId.

--- src/main/java/com/aldokenfack/SickleCareAI/service/PatientService.java
+++ src/main/java/com/aldokenfack/SickleCareAI/service/PatientService.java
@@ -4,16 +4,20 @@ import com.aldokenfack.SickleCareAI.config.JwtUtils;
 import com.aldokenfack.SickleCareAI.dto.PatientRegistrationDTO;
 import com.aldokenfack.SickleCareAI.dto.PatientResponseDTO;
 import com.aldokenfack.SickleCareAI.dto.PatientUpdateDTO;
+import com.aldokenfack.SickleCareAI.exception.AccountNotValidatedException;
 import com.aldokenfack.SickleCareAI.exception.UserAlreadyExistException;
 import com.aldokenfack.SickleCareAI.exception.UserNotFoundException;
+import com.aldokenfack.SickleCareAI.model.Doctor;
 import com.aldokenfack.SickleCareAI.model.Patient;
 import com.aldokenfack.SickleCareAI.model.Role;
 import com.aldokenfack.SickleCareAI.model.Validation;
+import com.aldokenfack.SickleCareAI.repository.DoctorRepository;
 import com.aldokenfack.SickleCareAI.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.AccessDeniedException;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +32,9 @@ public class PatientService {
     private final PasswordEncoder passwordEncoder;
     private final ValidationService validationService;
     private final JwtUtils jwtUtils;
+    private final DoctorRepository doctorRepository;
+    private final NotificationService notificationService;
+    private final AuthService authService;
 
 
     public PatientResponseDTO registerPatient(PatientRegistrationDTO dto){
@@ -87,41 +94,39 @@ public class PatientService {
     }
 
 
-    public PatientResponseDTO updatePatient(Long id, PatientUpdateDTO dto){
+    public PatientResponseDTO updatePatient(Long id, PatientUpdateDTO dto) throws AccessDeniedException {
 
-        Optional<Patient> patientToUpdate = patientRepository.findById(id);
+        // Fetch the id of the user who is authenticated
+        Long currentUserId = authService.getCurrentUser().getId();
 
-        Patient updatedPatient = null;
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
 
-        if (patientToUpdate.isPresent()){
-
-            Patient patient = patientToUpdate.get();
-
-            // Condition to update only the authorized field if isn't null
-            if (dto.getUsername() != null && !dto.getUsername().isEmpty()){
-                patient.setUsername(dto.getUsername());
-            }
-            if (dto.getEmail() != null && !dto.getEmail().isEmpty()){
-                patient.setEmail(dto.getEmail());
-            }
-            if (dto.getPassword() != null && !dto.getPassword().isEmpty()){
-                patient.setPassword(dto.getEmail());
-            }
-            if (dto.getWeight() != null && dto.getWeight() > 0){
-                patient.setWeight(dto.getWeight());
-            }
-            if (dto.getRegion() != null && !dto.getRegion().isEmpty()){
-                patient.setRegion(dto.getRegion());
-            }
-            if (dto.getCity() != null && !dto.getCity().isEmpty()){
-                patient.setCity(dto.getCity());
-            }
-
-            updatedPatient = patientRepository.save(patient);
-
-        } else {
-            throw new UserNotFoundException("Patient not found !");
+        if (!patient.getId().equals(currentUserId)){
+            throw new AccessDeniedException("You cannot update this user");
         }
+
+        // Condition to update only the authorized field if isn't null
+        if (dto.getUsername() != null && !dto.getUsername().isBlank()){
+            patient.setUsername(dto.getUsername());
+        }
+        if (dto.getEmail() != null && !dto.getEmail().isBlank()){
+            patient.setEmail(dto.getEmail());
+        }
+        if (dto.getPassword() != null && !dto.getPassword().isBlank()){
+            patient.setPassword(passwordEncoder.encode(dto.getEmail()));
+        }
+        if (dto.getWeight() != null && dto.getWeight() > 0){
+            patient.setWeight(dto.getWeight());
+        }
+        if (dto.getRegion() != null && !dto.getRegion().isBlank()){
+            patient.setRegion(dto.getRegion());
+        }
+        if (dto.getCity() != null && !dto.getCity().isBlank()){
+            patient.setCity(dto.getCity());
+        }
+
+        Patient updatedPatient = patientRepository.save(patient);
 
         return patientMapperService.mapToResponseDTO(updatedPatient);
     }
@@ -153,7 +158,51 @@ public class PatientService {
 
         patientRepository.save(patient);
 
-        return jwtUtils.generateToken(patient.getUsername());
+        return jwtUtils.generateToken(patient.getEmail());
+    }
+
+
+    public PatientResponseDTO validatePatient(Long id){
+
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("Patient not found"));
+
+        patient.setValidated(true);
+
+        this.patientRepository.save(patient);
+
+        // Send notification for successfully validation by admin
+        notificationService.sendAdminValidationSuccess(patient);
+
+        return patientMapperService.mapToResponseDTO(patient);
+    }
+
+
+    public PatientResponseDTO selectDoctor(Long patientId, Long doctorId) throws AccessDeniedException {
+
+        Long currentUserId = authService.getCurrentUser().getId();
+
+        // Verify that patient and doctor exists before applying selection
+        Patient patient = patientRepository.findById(patientId)
+                .orElseThrow(() -> new UserNotFoundException("Patient not found"));
+
+        Doctor doctor = doctorRepository.findById(doctorId)
+                .orElseThrow(() -> new UserNotFoundException("Doctor not found"));
+
+        if (!patientId.equals(currentUserId)){
+            throw new AccessDeniedException("You cannot select doctor for this user");
+        }
+
+        if (patient.getValidated() != true){
+            throw new AccountNotValidatedException("Your account is not yet validate by the administrator.\nPlease make sure to register all your information or contact the support");
+        }
+
+        patient.setDoctor(doctor);
+
+        Patient savedPatient = patientRepository.save(patient);
+
+        return patientMapperService.mapToResponseDTO(savedPatient);
+
     }
 
 }
