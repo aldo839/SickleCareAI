d3b03a09e7c6230a7d2f55ffc32cf78f4b3eb79c
aldo839 <christianaldo515@gmail.com>
2026-08-20 20:26:54 +0100

    Creation of the entity PasswordResetToken.

---
+++ src/main/java/com/aldokenfack/SickleCareAI/model/PasswordResetToken.java
@@ -0,0 +1,37 @@
+package com.aldokenfack.SickleCareAI.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private LocalDateTime expirationTime;
+
+
+    public PasswordResetToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+        this.expirationTime = LocalDateTime.now().plusMinutes(10);
+    }
+
+    public boolean isExpired(){
+        return LocalDateTime.now().isAfter(expirationTime);
+    }
+
+}
