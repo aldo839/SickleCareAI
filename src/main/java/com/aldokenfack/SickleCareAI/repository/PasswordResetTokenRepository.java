0774d54a8a0885c1a8f8c28f0110f496b280fb66
aldo839 <christianaldo515@gmail.com>
2026-08-20 20:30:09 +0100

    Creation of the PasswordResetTokenRepository with two methods, Optional findByToken and void deleteByUser

---
+++ src/main/java/com/aldokenfack/SickleCareAI/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,17 @@
+package com.aldokenfack.SickleCareAI.repository;
+
+import com.aldokenfack.SickleCareAI.model.PasswordResetToken;
+import com.aldokenfack.SickleCareAI.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    Optional<PasswordResetToken> findByToken(String token);
+
+    void deleteByUser(User user);
+
+}
