e2b25f43a7d10977b150248c34a855cd519b0b2f
aldo839 <christianaldo515@gmail.com>
2026-08-31 09:46:35 +0100

    Adding of one OpenApiCustomizer to remove security requirement on the public routes.

--- src/main/java/com/aldokenfack/SickleCareAI/config/OpenApiConfig.java
+++ src/main/java/com/aldokenfack/SickleCareAI/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 @Component
 public class OpenApiConfig {
@@ -31,6 +34,23 @@ public class OpenApiConfig {
                                         .description("Enter your token here.")
                                 )
                 );
+    }
+
+    @Bean
+    public OpenApiCustomizer publicRoutesCustomizer(){
+
+        return openAPI -> {
+            openAPI.getPaths().forEach((path, item) -> {
+                // Remove security requirement for those routes
+                if (path.startsWith("/api/auth") ||
+                path.equals("/api/patients/register-patient") ||
+                path.equals("/api/doctors/register-doctor") ||
+                path.equals("/api/admin/register-admin")) {
+
+                    item.readOperations().forEach(op -> op.setSecurity(Collections.emptyList()));
+                }
+            });
+        };
     }
 
 }
